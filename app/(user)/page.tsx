7077e3de0b56845fa54d87342ef35b0062caae22
Nived <nivuyt789@gmail.com>
2025-04-17 12:47:01 +0530

    added dynamic tag in layout

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,14 @@
 import Index from "../components/home/Index";
 
 export default async function Home() {
-  const response = await fetch(`${process.env.BASE_URL}/api/admin/home`, { next: { revalidate: 60 } });
-  const data = await response.json();
-  return (
-    <Index homeData={data.data} />
-  );
+  try {
+    const response = await fetch(`${process.env.BASE_URL}/api/admin/home`, { next: { revalidate: 60 } });
+    const data = await response.json();
+    return (
+      <Index homeData={data.data} />
+    );
+  } catch (error) {
+    console.log(error)
+  }
+
 }
--- app/(user)/services/[slug]/page.tsx
+++ app/(user)/services/[slug]/page.tsx
@@ -4,11 +4,16 @@ import React from 'react'
 
 
 export default async function ServicePage({params}: {params: Promise<{slug: string}>}){
-  const slug = (await params).slug
-  const response = await fetch(`${process.env.BASE_URL}/api/admin/service?slug=${slug}`,{next:{revalidate:60}})
-  const data = await response.json()
-  console.log(data)
-  return (
-        <Service data={data.data}/>
-    )
+  try {
+    const slug = (await params).slug
+    const response = await fetch(`${process.env.BASE_URL}/api/admin/service?slug=${slug}`,{next:{revalidate:60}})
+    const data = await response.json()
+    console.log(data)
+    return (
+          <Service data={data.data}/>
+      )
+  } catch (error) {
+    console.log(error)
+  }
+
 }
