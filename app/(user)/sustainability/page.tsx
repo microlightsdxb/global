f76dc16e5f906c748fb00bb8dc0cf87f362633b0
Nived <nivuyt789@gmail.com>
2025-04-17 12:49:05 +0530

    added try catch

--- app/(user)/sustainability/page.tsx
+++ app/(user)/sustainability/page.tsx
@@ -2,11 +2,16 @@ import Index from "../../components/Sustainability/Index";
 
 
 export default async function Home() {
-  const res = await fetch(`${process.env.BASE_URL}/api/admin/sustainability`,{next:{revalidate:60}});
-  const data = await res.json();
-  return (
-    <>
-    <Index data={data}/>
-    </>
-  );
+  try {
+    const res = await fetch(`${process.env.BASE_URL}/api/admin/sustainability`,{next:{revalidate:60}});
+    const data = await res.json();
+    return (
+      <>
+      <Index data={data}/>
+      </>
+    );
+  } catch (error) {
+    console.log(error)
+  }
+
 }
