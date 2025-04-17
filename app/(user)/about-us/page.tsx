89d7efad09c078250e7fe4d4c80650994dbb66b7
Nived <nivuyt789@gmail.com>
2025-04-17 12:44:18 +0530

    added dynamic tag in layout

--- app/(user)/about-us/page.tsx
+++ app/(user)/about-us/page.tsx
@@ -2,11 +2,16 @@ import Index from "../../components/AboutUs/Index";
 
 
 export default async function Home() {
-  const response = await fetch(`${process.env.BASE_URL}/api/admin/about`, { next: { revalidate: 60 } });
-  const data = await response.json();
-  return (
-    <>
-    <Index data={data.data} />
-    </>
-  );
+  try {
+    const response = await fetch(`${process.env.BASE_URL}/api/admin/about`, { next: { revalidate: 60 } });
+    const data = await response.json();
+    return (
+      <>
+      <Index data={data.data} />
+      </>
+    );
+  } catch (error) {
+    console.log(error)
+  }
+
 }
