65e38a848697197815d7fe223ff303e08d5db896
rahees <raheez1995india@gmail.com>
2025-06-10 12:15:49 +0530

    meta title and desc

--- app/(user)/terms-and-conditions/page.tsx
+++ app/(user)/terms-and-conditions/page.tsx
@@ -1,6 +1,12 @@
 import Index from '@/app/components/TermsnConditions'
 import React from 'react'
 
+export const metadata = {
+  title: 'Microlights Lighting LLC Website Terms and Conditions',
+  description:
+    'Welcome to our Website www.microlights.com (“Website”). This Website is operated by Microlights Lighting LLC (“Microlights”) (also referred to as “us”, “we”, “our” in these Terms and Conditions). Please read these Terms and Conditions carefully, they govern your access and use of the Website.',
+}
+
 export default function page() {
   return (
     <>
--- app/components/PrivacyPolicy/index.tsx
+++ app/components/PrivacyPolicy/index.tsx
@@ -1,17 +1,10 @@
-import Head from 'next/head'
 import ContentArea from './ContentArea'
 
 export default function index() {
 
   return (
-    <>
-      <Head>
-        <title>Microlights Lighting LLC – Privacy and Cookie Policy</title>
-        <meta name="description" content="This privacy and cookie policy sets out how we collect use, process and protect the information that we collect from you or you provide to us when you use our website.  " />
-      </Head>
       <main>
         <ContentArea />
       </main>
-    </>
   )
 }
