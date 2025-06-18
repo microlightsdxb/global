551415be138e9f7b340a59d09dd5feacafa1e2f6
jishnum007 <jishnu@globalsurf.ae>
2025-06-18 16:26:29 +0530

    co

--- app/components/ProjectDetails/sections/Pjctslider.tsx
+++ app/components/ProjectDetails/sections/Pjctslider.tsx
@@ -19,7 +19,8 @@ const Pjctslider = ({ data }: { data: { data: { images: string[] } } }) => {
           initial={{ opacity: 0, x: -50 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6 }}
-          viewport={{ once: false, amount: 0.5 }}
+        viewport={{ once: true, amount: 0.5 }}
+
       >
         <div className="relative">
           <Swiper
