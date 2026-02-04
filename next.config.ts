6cbdfbadca46415156bb7d9c76e0c7e8cba4ff2c
jishnum007 <jishnu@globalsurf.ae>
2026-02-04 14:39:32 +0530

    cache revert

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   async headers() {
-    return [
-      {
-        source: "/:all*(svg|jpg|png|webp|avif|gif|ico|woff2)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
+  //  async headers() {
+  //   return [
+  //     {
+  //       source: "/:all*(svg|jpg|png|webp|avif|gif|ico|woff2)",
+  //       headers: [
+  //         {
+  //           key: "Cache-Control",
+  //           value: "public, max-age=31536000, immutable",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   reactStrictMode: false,
   images: {
     dangerouslyAllowSVG:true,
