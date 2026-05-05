de4e4894f2cddf256568bc1096a3b283badc4077
jishnum007 <jishnu@globalsurf.ae>
2026-05-05 16:40:19 +0530

    adv tag

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -48,6 +48,19 @@ export default async function RootLayout({
           }}
           strategy="afterInteractive"
         />
+          <Script
+          id="gtm-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','AW-18111370184');
+            `,
+          }}
+        />
            <meta name="google-site-verification" content="LQa3Dbctxr8x55L9o0fNw6aURwCKHlA6oRBbjY0VWXE" />
       </head>
       <body className={`${parkinSans.variable} antialiased`}>
@@ -57,6 +70,14 @@ export default async function RootLayout({
             width="0"
             style={{ display: "none", visibility: "hidden" }}></iframe>
         </noscript>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/gtag/js?id=AW-18111370184`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>  
 
         {parse(tagData.tag.bodyScript)}
         <SmoothScroll />
